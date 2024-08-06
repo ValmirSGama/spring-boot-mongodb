39ffa5d6b2f513596995935ef1b0578d8e73a00c
Valmir Sales <valmir_salesgama@hotmail.com>
2024-08-06 19:13:08 -0300

    User update with PUT

--- src/main/java/com/valmirsales/mongodb/resources/UserResource.java
+++ src/main/java/com/valmirsales/mongodb/resources/UserResource.java
@@ -33,7 +33,7 @@ public class UserResource {
 		return ResponseEntity.ok().body(listDTO);
 	}
 	
-	@RequestMapping(value="{id}", method=RequestMethod.GET)
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
 	public ResponseEntity<UserDTO> findById(@PathVariable String id){
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(new UserDTO(obj));
@@ -47,9 +47,17 @@ public class UserResource {
 		return ResponseEntity.created(uri).build(); 
 	}
 	
-	@RequestMapping(value="{id}", method=RequestMethod.DELETE)
+	@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
 	public ResponseEntity<Void> delete(@PathVariable String id){
 		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.PUT)
+	public ResponseEntity<Void> update(@RequestBody UserDTO objDto, @PathVariable String id){
+		User obj = service.fromDTO(objDto);
+		obj.setId(id);
+		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
 	}
 }
